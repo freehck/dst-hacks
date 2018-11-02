e73681ddfa399d3259e3d382a0ffcb5ab16478b6
Dmitrii Kashin <freehck@freehck.ru>
2018-11-02 22:09:07 +0300

    bat file

---
+++ install.bat
@@ -0,0 +1,18 @@
+
+set DstModsDir=C:\Program Files (x86)\Steam\steamapps\common\Don't Starve Together\mods
+
+CALL :InstallMod local-coordinates, 727774324
+CALL :InstallMod camera-tweaks, 362906105, 1161850231
+CALL :InstallMod complete-your-map, 352373173, 459280938
+CALL :InstallMod quick-emotes, 369420067, 344071294
+
+EXIT /B %ERRORLEVEL%
+
+:InstallMod
+set "mod=%~1"
+set "id=%~2"
+set "extid=%~3"
+@RD /S /Q "%DstModsDir%\workshop-%id%"
+ROBOCOPY %mod% "%DstModsDir%\workshop-%id%" /E
+@RD /S /Q "%DstModsDir%\workshop-%extid%"
+EXIT /B 0
